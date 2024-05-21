154bbbd3c9e55d226e5d6dc8664d55740b7c4462
myjhye <t01087633799@gmail.com>
2024-05-21 23:05:11 +0900

    close logic

--- components/common/ModalContainer.tsx
+++ components/common/ModalContainer.tsx
@@ -1,4 +1,6 @@
-import { ReactNode } from "react";
+// 모달 창
+
+import { MouseEventHandler, ReactNode, useCallback, useEffect, useId } from "react";
 
 export interface ModalProps {
     visible?: boolean;
@@ -12,16 +14,47 @@ interface Props extends ModalProps {
 
 export default function ModalContainer({ visible, children, onClose }: Props) {
 
+    // 고유 id 
+    const containerId = useId();
+
+    // 모달 닫기, useCallback으로 메모이제이션
+    const handleClose = useCallback(() => {
+        onClose && onClose();
+    }, [onClose]);
+
+    // 모달 외부 클릭 시 모달 닫기
+    const handleClick:MouseEventHandler<HTMLDivElement> = (e: any) => {
+        if (e.target.id === containerId) {
+            handleClose();
+        }
+    };
+
+    useEffect(() => {
+        // 'Escape' 누르면 모달 닫기
+        const closeModal = (e: any) => {
+            if (e.key === 'Escape') {
+                handleClose();
+            }
+        };
+
+        // 'Escape' 누르면 모달 닫는 'keydown' 이벤트 리스너 추가
+        document.addEventListener('keydown', closeModal);
+
+        // 컴포넌트 언마운트 시(페이지 닫으면) 이벤트 리스너 제거
+        return () => {
+            document.removeEventListener("keydown", closeModal);
+        }
+    }, [handleClose]);
+
+    // 모달이 안 보이면 null
     if (!visible) {
         return null;
     }
 
-    const handleClick = () => {
-        onClose && onClose();
-    }
-
     return (
+        // 모달 외부 영역
         <div
+            id={containerId}
             onClick={handleClick} 
             className="fixed inset-0 bg-primary dark:bg-primary-dark dark:bg-opacity-5 bg-opacity-5 backdrop-blur-[2px] z-50 flex items-center justify-center"
         >
